1044b8058b765aaee9843d03a1dc382088fea486
Catreson <arekdagiel@gmail.com>
2024-01-05 13:52:06 +0100

    Added lapsplit

--- wsrta.py
+++ wsrta.py
@@ -96,7 +96,7 @@ class LFSplit(LFGener):
         LFGener.__init__(self, master, filetype = '.wsrt', filedescription = 'WSRT Files', titel = 'Split', *args, **kwargs)
     
     def buttonAction(self):
-        SplitWindowRider(self, self.path)   
+        SplitWindow(self, self.path)   
 class LFView(LFGener):
     def __init__(self, master, *args, **kwargs):
         LFGener.__init__(self, master, filetype = '.wsrtses', filedescription = 'WSRT Session Files', titel = 'Preview', *args, **kwargs)
@@ -186,115 +186,199 @@ class ParseWindowWSRT(tk.Toplevel):
                 raise
         else:
             self.label_state.config(text="Invalid file",fg="red")
-
-class SplitWindowRider(tk.Toplevel):
+class RiderInfo(tk.Frame):
     def __init__(self, master, path, *args, **kwargs):
-        tk.Toplevel.__init__(self, master, *args, **kwargs)
+        tk.Frame.__init__(self, master, *args, **kwargs)
+        self.master = master
         self.path = path
-        self.session_metadata = {}
-        self.click_count = 0
-        self.frame = tk.Frame(self,
-                              height = 600,
-                              width=250)
-        self.frame.pack()
-        self.geometry('600x250')
-        self.label_rider = tk.Label(self.frame, text="Input rider name: ")
-        self.input_rider = tk.Entry(self.frame, width=30)
-        self.label_track = tk.Label(self.frame, text="Input track name: ")
-        self.input_track = tk.Entry(self.frame, width=30)
-        self.label_date = tk.Label(self.frame, text="Input date: ")
-        self.label_date_selected = tk.Label(self.frame, text=datetime.today().strftime('%Y-%m-%d'))
+        self.config(height=600,
+                    width=250)
+        
+        self.label_rider = tk.Label(self, text="Input rider name: ")
+        self.input_rider = tk.Entry(self, width=30)
+        self.label_track = tk.Label(self, text="Input track name: ")
+        self.input_track = tk.Entry(self, width=30)
+        self.label_date = tk.Label(self, text="Input date: ")
         self.date_val = tk.StringVar()
-        self.cal = Calendar(self.frame, selectmode='day',
+        self.cal = Calendar(self, selectmode='day',
                    date_pattern='yyyy-mm-dd',
                    year=int(datetime.today().strftime('%Y')),
                    month=int(datetime.today().strftime('%m')),
                      day=int(datetime.today().strftime('%d')),
                      textvariableStringVar=self.date_val)
-        self.label_id = tk.Label(self.frame, text="Input session ID\n(to make identification easier)")
-        self.input_id = tk.Entry(self.frame, width=30)
+        self.label_id = tk.Label(self, text="Input session ID\n(to make identification easier)")
+        self.input_id = tk.Entry(self, width=30)
 
         self.label_rider.grid(sticky="W",column=0,row=0)
         self.input_rider.grid(sticky="W", column=1, row=0)
-        self.cal.grid(column=3,row=0,rowspan=5)
         self.label_track.grid(sticky="W",column=0,row=1)
         self.input_track.grid(stick="W", column=1, row=1)
         self.label_date.grid(sticky="W",column=0,row=2)
-        self.label_date_selected.grid(sticky="W",column=1, row=2)
+        self.cal.grid(column=1,row=2, rowspan = 2)
         self.label_id.grid(sticky="W", column=0, row=4)
         self.input_id.grid(sticky="W",column=1, row=4)
-        self.cp = SmallControls(self, buttonContinue= self.getLine)
-        self.cp.pack(anchor=tk.S)
     
-    def getLine(self):
-        self.session_metadata['session_rider'] = self.input_rider.get()
-        self.session_metadata['session_track'] = self.input_track.get()
-        self.session_metadata['session_date'] = self.label_date_selected.cget('text')
-        self.session_metadata['session_id'] = self.input_id.get()
-        self.frame.destroy()
-        self.cp.destroy()
-        self.geometry('700x320')
-        self.frame = tk.Frame(self,
-                              height = 700,
-                              width=320)
-        self.frame.pack()
-        self.cp = SmallControls(self,
-                                 buttonContinue= self.splitLaps)
-        self.cp.pack(anchor=tk.S)
+    def buttonContinue(self):
+        self.master.session_metadata['session_rider'] = self.input_rider.get()
+        self.master.session_metadata['session_track'] = self.input_track.get()
+        self.master.session_metadata['session_date'] = str(self.cal.selection_get())
+        self.master.session_metadata['session_id'] = self.input_id.get()
+        self.master.geometry('700x320')
+        print(self.master.session_metadata)
+class FnishlineInfo(tk.Frame):
+    def __init__(self, master, path, *args, **kwargs):
+        tk.Frame.__init__(self, master, *args, **kwargs)
+        self.master = master
+        self.path = path
+        self.click_count = 0
+        self.config(height=600,
+                    width=250)
+        self.data = pd.read_csv(self.path)
 
-    def splitLaps(self):
-        data = pd.read_csv(self.path)
-        t_lat = data['latitude'][50]
-        t_lon = data['longitude'][50]
+        self.t_lat = self.data['latitude'][50]
+        self.t_lon = self.data['longitude'][50]
 
-        label_1 = tk.Label(self.frame, text = "1st point (latitude, longitude): ")
-        label_2 = tk.Label(self.frame, text = "2nd point (latitude, longitude): ")
-        label_1.config(text=str(t_lat))
-        label_2.config(text=str(t_lon))
-        input_lat_1 = tk.Entry(self.frame, 
+        self.label_1 = tk.Label(self, text = "1st point (latitude, longitude): ")
+        self.label_2 = tk.Label(self, text = "2nd point (latitude, longitude): ")
+        self.label_1.config(text=str(self.t_lat))
+        self.label_2.config(text=str(self.t_lon))
+        self.input_lat_1 = tk.Entry(self, 
                width = 10) 
-        input_lon_1 = tk.Entry(self.frame, 
+        self.input_lon_1 = tk.Entry(self, 
                width = 10) 
-        input_lat_2 = tk.Entry(self.frame, 
+        self.input_lat_2 = tk.Entry(self, 
                width = 10) 
-        input_lon_2 = tk.Entry(self.frame, 
+        self.input_lon_2 = tk.Entry(self, 
                width = 10) 
-        input_lat_1.insert(0, 51.417095)
-        input_lon_1.insert(0, 21.149420)
-        input_lat_2.insert(0, 51.417186)
-        input_lon_2.insert(0, 21.149489)
-        label_tolerance = tk.Label(self.frame,
+        self.input_lat_1.insert(0, 51.417095)
+        self.input_lon_1.insert(0, 21.149420)
+        self.input_lat_2.insert(0, 51.417186)
+        self.input_lon_2.insert(0, 21.149489)
+        self.label_tolerance = tk.Label(self,
                                 text = "Set splitting tolerance: \n(as fraction of finish line length)")
-        input_tolerance = tk.Entry(self.frame, 
+        self.input_tolerance = tk.Entry(self, 
                width = 4) 
-        input_tolerance.insert(0, 0.1)
-        label_minimal = tk.Label(self.frame,
+        self.input_tolerance.insert(0, 0.1)
+        self.label_minimal = tk.Label(self,
                               text = "Set minimal laptime in seconds:")
-        input_minimal = tk.Entry(self.frame, 
+        self.input_minimal = tk.Entry(self, 
                                 width = 4) 
-        input_minimal.insert(0, 25)
+        self.input_minimal.insert(0, 25)
+        self.map_widget = tkm.TkinterMapView(self, width=400, height=300, corner_radius=0)
+        self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+        self.map_widget.set_position(self.t_lat, self.t_lon)
+        self.map_widget.add_right_click_menu_command(label="Add 1st point",
+                                            command=self.set1stPoint,
+                                            pass_coords=True)
+        self.map_widget.add_right_click_menu_command(label="Add 2nd point",
+                                            command=self.set2ndPoint,
+                                            pass_coords=True)
+        self.map_widget.add_left_click_map_command(self.leftClick)
+
+        self.label_1.grid(sticky="W", column=0, row=0)
+        self.label_2.grid(sticky="W", column=0, row=1)
+        self.input_lat_1.grid(sticky="W", column=1, row = 0)
+        self.input_lon_1.grid(sticky="W", column=2, row = 0)
+        self.input_lat_2.grid(sticky="W", column=1, row = 1)
+        self.input_lon_2.grid(sticky="W", column=2, row = 1)
+        self.label_tolerance.grid(sticky="W", column=0, row=2)
+        self.input_tolerance.grid(sticky="W", column=1, row=2)
+        self.label_minimal.grid(sticky="W", column=0, row=3)
+        self.input_minimal.grid(sticky="W", column=1, row=3)
+        self.map_widget.grid(sticky="NE", column=3,row=0, rowspan=5)
 
     def set2ndPoint(self, coords):
-        input_lat_2.delete(0, tk.END)
-        input_lon_2.delete(0, tk.END)
-        input_lat_2.insert(0, coords[0])
-        input_lon_2.insert(0, coords[1])
-        set2ndPoint.path1 = map_widget.set_path([(float(input_lat_1.get()), float(input_lon_1.get())), (coords[0], coords[1])])
+        self.input_lat_2.delete(0, tk.END)
+        self.input_lon_2.delete(0, tk.END)
+        self.input_lat_2.insert(0, coords[0])
+        self.input_lon_2.insert(0, coords[1])
+        self.path1 = self.map_widget.set_path([(float(self.input_lat_1.get()), float(self.input_lon_1.get())), (coords[0], coords[1])])
     def set1stPoint(self, coords):
-        input_lat_1.delete(0, tk.END)
-        input_lon_1.delete(0, tk.END)
-        input_lat_1.insert(0, coords[0])
-        input_lon_1.insert(0, coords[1])
+        self.input_lat_1.delete(0, tk.END)
+        self.input_lon_1.delete(0, tk.END)
+        self.input_lat_1.insert(0, coords[0])
+        self.input_lon_1.insert(0, coords[1])
         try:
-            set2ndPoint.path1.delete()
+            self.path1.delete()
         except:
             pass
     def leftClick(self, coords):
-        if click_count == 0:
-            set1stPoint(coords)
-        elif click_count == 1:
-            set2ndPoint(coords)
-        click_count = (click_count + 1) % 2
+        self.click_count
+        if self.click_count == 0:
+            self.set1stPoint(coords)
+        elif self.click_count == 1:
+            self.set2ndPoint(coords)
+        self.click_count = (self.click_count + 1) % 2
+
+    def createSessionFile(self, laps):
+        p = Path(self.path)
+        laps_path = Path.cwd() / p.stem
+        if not os.path.isdir(laps_path):
+            os.mkdir(laps_path)
+        with open(laps_path / f'{p.stem}.wsrtses', 'wb') as file:
+            rut = ET.Element('session')
+            details = ET.SubElement(rut, 'details')
+            laplist = []
+            laptimes = ET.SubElement(rut, 'laptimes')
+            rider_name = ET.SubElement(details, 'rider_name')
+            rider_name.text = self.master.session_metadata['session_rider']
+            track_name = ET.SubElement(details, 'track_name')
+            track_name.text = self.master.session_metadata['session_track']
+            ride_date = ET.SubElement(details, 'date')
+            ride_date.text = self.master.session_metadata['session_date']
+            ride_id = ET.SubElement(details, 'identifier')
+            ride_id.text = self.master.session_metadata['session_id']
+
+            no_laps = ET.SubElement(laptimes, 'no_laps')
+            no_laps.text = str(len(laps))
+            for i, lap in enumerate(laps, 1):
+                tmp = ET.SubElement(laptimes, f'lap')
+                tmp.set('number', f'l{i}')
+                tmp.text = str(lap[1] - lap[0])
+                laplist.append(tmp)
+            tmp = minidom.parseString(ET.tostring(rut)).toprettyxml(indent="   ")
+            file.write(tmp.encode('utf-8'))
+        return laps_path
+
+    def buttonContinue(self):
+        start_line = [(float(self.input_lon_1.get()), float(self.input_lat_1.get())), (float(self.input_lon_2.get()), float(self.input_lat_2.get()))]
+        minimal_laptime = float(self.input_minimal.get())
+        finish_line_tolerance = float(self.input_tolerance.get())
+        self.master.master.label_processing.config(text="Processing...",fg="yellow")
+        laps = find_laps(self.data, start_line, minimal_laptime, finish_line_tolerance)
+        laps_path = self.createSessionFile(laps)
+        laps_data = split_laps(self.data, laps)
+        for i, lap in enumerate(laps_data):
+            lap.to_csv(f'{laps_path}/l{i+1}.lap', index=False)
+        self.master.master.label_processing.config(text="Complete",bg="green")
+class SplitWindow(tk.Toplevel):
+    def __init__(self, master, path, *args, **kwargs):
+        tk.Toplevel.__init__(self, master, *args, **kwargs)
+        self.master = master
+        self.path = path
+        self.session_metadata = {}
+        self.frames = []
+        self.frames.append(RiderInfo(self, self.path))
+        self.frames.append(FnishlineInfo(self, self.path))
+
+        self.frames[0].pack()
+        self.cp = SmallControls(self, buttonContinue=self.buttonContinue)
+        self.cp.pack()
+
+    def buttonContinue(self):
+        if len(self.frames):
+            self.cp.pack_forget()
+            self.frames[0].buttonContinue()
+            self.frames[0].destroy()
+            del self.frames[0]
+        if len(self.frames):
+            self.frames[0].pack()
+            self.cp.pack()
+        else:
+            self.destroy()
+
+class ViewWindow(tk.Toplevel):
+    pass
 
 class MainWindow():
     def __init__(self, master, *args, **kwargs):
